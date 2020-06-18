4a43ec38cea941a48a1140c9cb2fe029201db677
Lukas Koch <lukas.koch@mailbox.org>
2020-06-18 22:57:16 +0100

    Implement TCanvas.

--- src/tcanvas/tcanvas.py
+++ src/tcanvas/tcanvas.py
@@ -155,9 +155,11 @@ class TCanvas(object):
 
     columns, rows : int or None
         The size of the canvas. Defaults to current terminal size.
+    **kwargs : optional
+        Properties of the Texels to initialise the canvas.
     """
 
-    def __init__(self, columns=None, rows=None):
+    def __init__(self, columns=None, rows=None, **kwargs):
         term_width, term_height = click.get_terminal_size()
         if columns is None:
             self._columns = term_width
@@ -168,17 +170,137 @@ class TCanvas(object):
         else:
             self._rows = rows
 
-        self.clear_buffer()
+        self.clear(**kwargs)
 
-    def get_size(self):
+        self._transformations = {}
+
+    @property
+    def size(self):
         """Return the number of columns and rows."""
         return self._columns, self._rows
 
-    def clear():
-        """Clear the canvas."""
+    def clear(self, **kwargs):
+        """Clear the canvas.
+
+        Parameters
+        ----------
+
+        **kwargs : optional
+            Initialise the Texels with these settings.
+
+        """
         self._buffer = []
-        nrow, ncol = self.get_size()
+        ncol, nrow = self.size
         for row in range(nrow):
             self._buffer.append([])
             for column in range(ncol):
-                self._buffer[-1].append(Texel())
+                self._buffer[-1].append(Texel(**kwargs))
+
+    @property
+    def buffer(self):
+        """Return the buffer of the canvas."""
+        return self._buffer
+
+    @property
+    def transformations(self):
+        """Return the named transformation functions."""
+        return self._transformations
+
+    def transform_position(self, pos, transformation=None):
+        """Transform a floating point position to a texel index.
+
+        Returns
+        -------
+
+        col, row : int
+
+        """
+
+        # No transformation:
+        # x = columns from the left
+        # y = rows from the top
+        if transformation is None:
+            return tuple(int(round(x)) for x in pos)
+
+        # Look up transformation
+        return self._transformations[transformation](pos)
+
+    def render(self):
+        """Render the canvas as a string."""
+        string = ""
+        for line in self.buffer:
+            for texel in line:
+                string += texel.render()
+            string += "\n"
+        # Remove last newline
+        return string[:-1]
+
+    def show(self):
+        """Print the canvas to the screen.
+
+        This does not print a final newline.
+
+        """
+
+        click.echo(self.render())
+
+    def set(self, pos, transformation=None, **kwargs):
+        """Set texel properties at the given position.
+
+        Parameters
+        ----------
+
+        pos : (int, int) or (float, float)
+            The position of the manipulated texel
+        transformation : str, optional
+            The transformation to be used to translate between the position
+            coordinates and text rows and columns
+        **kwargs : optional
+            The properties of the Texel to be set
+
+        """
+
+        col, row = self.transform_position(pos, transformation)
+        ncol, nrow = self.size
+        if 0 <= col < ncol and 0 <= row < nrow:
+            for attr, val in kwargs.items():
+                setattr(self._buffer[row][col], attr, val)
+
+    def line(self, pos0, pos1, transformation=None, **kwargs):
+        """Draw a line between the two positions."""
+
+        px0, py0 = pos0
+        px1, py1 = pos1
+        x0, y0 = self.transform_position(pos0, transformation)
+        x1, y1 = self.transform_position(pos1, transformation)
+
+        Dx = abs(x1 - x0)
+        Dy = abs(y1 - y0)
+        steps = max(Dx, Dy)
+
+        if steps == 0:
+            self.set(pos0, transformation, **kwargs)
+        else:
+            dpx = (px1 - px0) / steps
+            dpy = (py1 - py0) / steps
+            x = px0
+            y = py0
+            self.set((x, y), transformation, **kwargs)
+
+            for i in range(steps):
+                x += dpx
+                y += dpy
+                self.set((x, y), transformation, **kwargs)
+
+    def text(self, pos, text, transformation=None, **kwargs):
+        """Write some text at the specified position."""
+
+        x0, y0 = self.transform_position(pos, transformation)
+        x, y = x0, y0
+        for char in text:
+            if char == "\n":
+                y += 1
+                x = x0
+            else:
+                self.set((x, y), transformation=None, character=char, **kwargs)
+                x += 1
--- src/tcanvas/test.py
+++ src/tcanvas/test.py
@@ -25,10 +25,21 @@ def test_texel():
     echo("Blink c.:" + Texel("B", blink=True, fg_color="b", bg_color="y").render())
 
 
+def test_tcanvas():
+    c = TCanvas(columns=20, rows=10, bg_color="K")
+    c.text((13, 0), "=======\nTCanvas\n=======")
+    c.set((0, 0), character="*")
+    c.set((19, 9), character="#")
+    c.line((-1, 5), (10, -2), bg_color="r")
+    c.line((2, -1), (6, 4), fg_color="W", blink=True, character="x")
+    c.show()
+
+
 def main():
     """Run some tests."""
 
     test_texel()
+    test_tcanvas()
 
 
 if __name__ == "__main__":
